7566da75a76ba1e27fdc7bf92e8e123cdbc34478
Seagullible <natlockhart@gmail.com>
2022-10-01 08:16:13 -0600

    Initial commit with Angular site

---
+++ survey-software/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    AppHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
